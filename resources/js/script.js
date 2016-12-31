09da9340e7fa1ee5a2536341676ebcc6e1bda238
John Erickson <ericksonjohn08@hotmail.com>
2016-12-31 14:45:04 -0500

    Corrected JQuery errors on sticky navigation

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,13 +1,18 @@
-$(document).ready(function() {
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+$(document).ready(function () {
+	"use strict";
 	
-	$('.js--section-features').waypoint(function(direction) {
-		if (direction == "down") {
+	/* Sticky Navigation */
+	$('.js--section-features').waypoint(function (direction) {
+		if (direction === "down") {
 			$('nav').addClass('sticky');
 		} else {
 			$('nav').removeClass('sticky');
-		}																		
+		}
 	}, {
 		offset: '60px'
 	});
-							 
+	
 });
